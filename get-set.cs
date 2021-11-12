1b07f7f0745f990dcde02157c6588fb36ad43d83
Arafat Hasan <opendoor.arafat@gmail.com>
2021-11-12 20:46:14 +0600

    get set

---
+++ get-set.cs
@@ -0,0 +1,16 @@
+using System;
+class Person
+{
+    public string Name  // property
+    { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person myObj = new Person();
+        myObj.Name = "Liam";
+        Console.WriteLine(myObj.Name);
+    }
+}
